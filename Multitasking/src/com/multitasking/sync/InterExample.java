7fe629938abedf25de69db287b1a551621fef61c
Ajay Ingle <ajay.ingle@vit.edu.in>
2022-04-19 18:29:01 +0530

    code change

---
+++ Multitasking/src/com/multitasking/sync/InterExample.java
@@ -0,0 +1,67 @@
+package com.multitasking.sync;
+class Postman extends Thread{
+	int sum=0;
+	@Override
+	public void run() {
+		synchronized (this) {
+			System.out.println("Postman Thread starts calculations : " + Thread.currentThread().getName());
+			for(int i=0;i<=100;i++) {
+				sum = sum + i;
+			}
+			System.out.println("Postman thread trying to give notification");
+			this.notify();
+		}
+	}
+}
+
+class Boy extends Thread{
+	
+	Postman p = new Postman();
+	
+	@Override
+	public void run() {
+		 p.start();
+		
+		//1.sleep method - not recommded- Postman has done his notification but Boy sleep for exta n millisec -> not get ans 
+// System.out.println("Boy Thread Trying to call wait() method : " + Thread.currentThread().getName());
+//		try {
+//			Thread.sleep(1000);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		System.out.println("Boy thread gets notification and value after Postman completes : " + p.sum);
+		
+		// 2. join method - not recommended - in postman code we have before and after lot of code then we need to do wait until this code finish 
+//        System.out.println("Boy Thread Trying to call wait() method : " + Thread.currentThread().getName());
+//		try {
+//			p.join();
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//        System.out.println("Boy thread gets notification and value after Postman completes : " + p.sum);
+
+
+		synchronized (p) { 
+			System.out.println("Boy Thread Trying to call wait() method : " + Thread.currentThread().getName());
+			try {
+				p.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("Boy thread gets notification and value after Postman completes : " + p.sum);
+		}
+	}
+	
+}
+
+public class InterExample {
+	
+	// 1. wait, notify and notifyAll() →  used in object and not in thread
+	//2. These methods only call inside sync area only because every thread is owner of any object. and each thread has lock.
+	
+
+	public static void main(String[] args) {
+		Boy b = new Boy();
+		b.start();
+	}
+}
--- Multitasking/src/com/multitasking/sync/SyncWithSameObject.java
+++ Multitasking/src/com/multitasking/sync/SyncWithSameObject.java
@@ -7,6 +7,13 @@ public class SyncWithSameObject {
 		// one object - c1
 		// three threads - s1,s2,s3
 		
+		// 1. It is only for method and block but not for class and variable
+		// 2. better option where we want to allow only one thread to access the shared resource.
+		// 3. Disadv - it increases waiting time-of the Thread and effects performance of the system.
+		// 4. Internally synchronization concept is implemented by using lock concept.
+		// 5. Thread wants to do opeartions on object. -> Thread will get lock and enter into section - relase lock when completed
+		
+		
 		Cricketer c1 = new Cricketer();
 	    SendMessage s1 = new SendMessage("Dhoni", c1);
 	    SendMessage s2 = new SendMessage("Sachin", c1);
