d708b84aff99b2f63edba766df32ca9471287d5e
Ajay Ingle <83777823+ingleajay@users.noreply.github.com>
2022-06-02 21:03:26 +0530

    Update Variable.java
    code changes

--- Basic Java/src/com/basic/Variable.java
+++ Basic Java/src/com/basic/Variable.java
@@ -8,15 +8,17 @@ public class Variable {
 	
 	public static void main(String[] args) {
 		
-		// use of local variable -> within class or methods and access variable directly
+		// use of local variable -> A variable declared inside the body of the method is called local variable.
 		int c = 30; 
 		System.out.println("Access local vaiable: " + c );
 
-		// use of instance variable -> inside a class and access through object
+		// use of instance variable ->A variable declared inside the class but outside the body of the method, is called an instance variable
 		Variable v = new Variable();
 		System.out.println("Access instance variable: " + v.a);
 		
 	    // use of static variable -> memory management and no need of object
+		// -> You can create a single copy of the static variable and share it among all the instances of the class.
+		// -> Memory allocation for static variables happens only once when the class is loaded in the memory	
 		System.out.println("Access static variable: " + b);
 
 	}
